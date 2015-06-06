f0fbd63106f70bb7f2a5c14b1022e38ac2c76505
eaton11 <sameaton11@gmail.com>
2015-06-06 13:06:25 -0600

    adds many new feature and updates (see details)
    changes "addText" to "text"
    changes "appendEl" to "append"
    makes append and appendTo combatible with an array of elements
    resolves any purging and removing element conflicts
    adds the ability to create elements
    adds the ability to create multiple elements
    adds the ability to specify how many elements to select, 2nd param

--- el.js
+++ el.js
@@ -1,22 +1,42 @@
 /*!
 	<el> by samr_eaton
 */
-var el = function(_string){
-	function getElement(_string){
+var el = function(_string, _num){
+	if(typeof _num === "undefined") _num = null;
+	function getElement(_string, _num){
 		var firstChar = _string.charAt(0);
-		if(firstChar === "#"){
-			return document.getElementById(_string.slice(1));
-		} else if(firstChar === ".") {
-			var elements = document.getElementsByClassName(_string.slice(1));
-			elements.arr = true;
-			return elements;
-		} else { //tag name
-			var elements = document.getElementsByTagName(_string);
-			elements.arr = true;
-			return elements;
+		switch (firstChar){
+			case "#":
+				return document.getElementById(_string.slice(1));
+			case "+":
+				if(_num && typeof _num === "number"){ // multi
+					var elements = [];
+					elements.arr = true;
+					for(var i = 0, ii = _num; i < ii; i++){
+						elements.push(document.createElement(_string.slice(1)));
+					}
+					return elements;
+				}
+				return document.createElement(_string.slice(1)); // single
+			case ".":
+				var elements = document.getElementsByClassName(_string.slice(1));
+				elements.arr = true;
+				if(_num && typeof _num === "number"){ 
+					return elements.slice(0,_num); 
+				}
+				return elements;
+			default:
+				var elements = document.getElementsByTagName(_string);
+				elements.arr = true;
+				if(_num && typeof _num === "number"){ 
+					return elements.slice(0,_num); 
+				}
+				return elements;
 		}
 	}
 	function addMethods(_obj){
+
+
 		// ==== adds methods to multi element arr ====
 		if(_obj.arr === true){
 
@@ -33,34 +53,42 @@ var el = function(_string){
 
 			_obj.addClass = function(_class){
 				_obj.each(function(_item){
-					if(!_item.el) _item = addMethods(_item);
-					_item.addClass(_class);
+					if(typeof _item !== "undefined"){
+						if(!_item.el) _item = addMethods(_item);
+						_item.addClass(_class);
+					}
 				})
 				return _obj;
 			}
 
 			_obj.rmClass = function(_class){
 				_obj.each(function(_item){
-					if(!_item.el) _item = addMethods(_item);
-					_item.addClass(_class);
+					if(typeof _item !== "undefined"){	
+						if(!_item.el) _item = addMethods(_item);
+						_item.addClass(_class);
+					}
 				})
 				return _obj;
 			}
 
 			_obj.hasClass = function(_class, _which){
-				if(_which === "all"){
+				if(_which === "all" || typeof _which === "undefined"){
 					_obj.each(function(_item){
-						if(!_item.el) _item = addMethods(_item);
-						// if ANY trigger the if statement then not ALL have the class
-						if (!_item.addClass(_class)) return false;
+						if(typeof _item !== "undefined"){	
+							if(!_item.el) _item = addMethods(_item);
+							// if ANY trigger the if statement then not ALL have the class
+							if (!_item.addClass(_class)) return false;
+						}
 					});
 					// none triggered the if statement
 					return true; 
 				} else if(_which === "any"){
 					_obj.each(function(_item){
-						if(!_item.el) _item = addMethods(_item);
-						// if ANY trigger the if statement then one has the class
-						if (_item.addClass(_class)) return true;
+						if(typeof _item !== "undefined"){
+							if(!_item.el) _item = addMethods(_item);
+							// if ANY trigger the if statement then one has the class
+							if (_item.addClass(_class)) return true;
+						}	
 					});
 					// none triggered the if statement
 					return false; 
@@ -70,46 +98,70 @@ var el = function(_string){
 
 			_obj.rm = function(_class){
 				_obj.each(function(_item){
-					if(!_item.el) _item = addMethods(_item);
-					_item.rm();
+					if(typeof _item !== "undefined"){
+						if(!_item.el) _item = addMethods(_item);
+						_item.rm();
+					}	
 				})
 				return _obj;
 			}
 
-			_obj.appendEl = function(_el){
+			_obj.append = function(_el){
+				// if incoming is array, convert to fragment
+				if(_el.arr){
+					var fragment = document.createDocumentFragment();
+					_el.each(function(single){
+						if(!single.el) single = addMethods(single);
+						fragment.appendChild(single);
+					});
+					_el = fragment;
+				}
+
 				_obj.each(function(_item){
-					if(!_item.el) _item = addMethods(_item);
-					_item.appendEl(_el);
+					if(typeof _item !== "undefined"){
+						if(!_item.el) _item = addMethods(_item);
+						_item.append(_el.cloneNode(true));
+					}	
 				})
+
 				return _obj;
 			}
+
 
 			_obj.appendTo = function(_el){
+
 				_obj.each(function(_item){
-					if(!_item.el) _item = addMethods(_item);
-					_el.appendEl(_item);
+					if(typeof _item !== "undefined"){
+						if(!_item.el) _item = addMethods(_item);
+						_el.append(_item);
+					}	
 				})
 				return _obj;
 			}
 
-
-			_obj.addText = function(_string){
+			_obj.text = function(_string){
 				_obj.each(function(_item){
-					if(!_item.el) _item = addMethods(_item);
-					_item.addText(_string);
+					if(typeof _item !== "undefined"){
+						if(!_item.el) _item = addMethods(_item);
+						_item.text(_string);
+					}
 				})
 				return _obj;
 			}
 
 			_obj.purge = function(){
 				_obj.each(function(_item){
-					if(!_item.el) _item = addMethods(_item);
-					_item.purge();
+					if(typeof _item !== "undefined"){
+						if(!_item.el) _item = addMethods(_item);
+						_item.purge();
+					}
 				})
 				return _obj;
 			}
 			return _obj;
-		}
+		} // END ARRAY METHODS
+
+
 
 
 		// ==== adds methods to single element ====
@@ -165,18 +217,34 @@ var el = function(_string){
 			return self;
 		};
 
-		_obj.appendEl = function(_el){
+		_obj.append = function(_el){
+			if(_el.arr){
+				var fragment = document.createDocumentFragment();
+				_el.each(function(single){
+					if(!single.el) single = addMethods(single);
+					fragment.appendChild(single);
+				});
+				_el = fragment;
+			}
 			this.appendChild(_el);
 			return this;
 		};
 
 		_obj.appendTo = function(_el){
 			var self = this;
-			_el.appendChild(self);
+			// _el.appendChild(self);
+			// console.log(_el);
+			if(_el.arr){
+				_el.each(function(eachEl){
+					eachEl.appendChild(self.cloneNode(true));
+				})
+			} else {
+				_el.appendChild(self);
+			}
 			return self;
 		};
 
-		_obj.addText = function(_string){
+		_obj.text = function(_string){
 			if(typeof _string === "string")
 				this.appendChild(document.createTextNode(_string));
 			return this;
@@ -188,10 +256,12 @@ var el = function(_string){
 			  self.removeChild(self.firstChild);
 			return self;
 		}
+		
+
 
 		return _obj;
 	}
-	var newObject = getElement(_string);
+	var newObject = getElement(_string, _num);
 	newObject = addMethods(newObject);
 	return newObject;
 }
